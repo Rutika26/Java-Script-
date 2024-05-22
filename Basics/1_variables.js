9c0a21d6aeb727173a648dd3978d4e045e8269cf
Rutika26 <48618970+Rutika26@users.noreply.github.com>
2024-05-22 10:50:53 +0000

    container config done

---
+++ Basics/1_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "rutika@google.com"
+var accountPassword = "12345"
+accountCity = "pune"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "rutika@gmail.com"
+accountPassword = "123456"
+accountCity = "Maharashtra"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
